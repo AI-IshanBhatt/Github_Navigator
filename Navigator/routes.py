34ba9382b59fa6a93cb0a2881d48556959d86381
Ishan Bhatt <ishanbhatt@halwa>
2018-06-18 11:34:52 +0530

    example.html page working, need to add logger and gunicorn stuff

--- Navigator/routes.py
+++ Navigator/routes.py
@@ -1,5 +1,5 @@
 from Navigator import app
-from flask import request, jsonify
+from flask import request, jsonify, render_template
 import settings.development as settings
 from .utils import get_repositories
 
@@ -7,11 +7,14 @@ from .utils import get_repositories
 # Root Routes
 #-------------------------------
 
+
 @app.route('/navigator')
 def get_repos(page_size=settings.PAGE_SIZE, order=settings.ORDER):
     search_term = request.args.get('search_term')
-    return jsonify(get_repositories(search_term, page_size, order))
+    # return jsonify(get_repositories(search_term, page_size, order))
+    repositories = get_repositories(search_term, page_size, order)
+    return render_template("ReposList.html", repo_list=repositories, search_term=search_term)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html')
--- Navigator/utils.py
+++ Navigator/utils.py
@@ -2,12 +2,13 @@ import requests
 import json
 from operator import itemgetter
 import re
+from time import strftime
 
 REPO_SEARCH_URL = "https://api.github.com/search/repositories?q={0}+in:name"
 
 
 def get_repositories(search_term, page_size, order):
-    r = requests.get(REPO_SEARCH_URL.format(search_term))
+    r = requests.get(REPO_SEARCH_URL.format(search_term), proxies={'http': "proxy.uk.labs.sophos:8080", 'https': "proxy.uk.labs.sophos:8080"}) #Remove proxies
     content = json.loads(r.content.decode(encoding="utf-8"))
 
     refined_repos = []
@@ -26,15 +27,18 @@ def get_repositories(search_term, page_size, order):
 def add_commit_details(refined_repos):
     for refined_repo in refined_repos:
         commits_url = re.sub(r"{/sha}", r"/master", refined_repo["commits_url"])
-        r = requests.get(commits_url)
+        r = requests.get(commits_url, proxies={'http': "proxy.uk.labs.sophos:8080", 'https': "proxy.uk.labs.sophos:8080"})
         content = json.loads(r.content.decode(encoding="utf-8"))
         del refined_repo["commits_url"]
-        refined_repo.update({'last_commit_sha': content['sha'], 'last_commit_message': content["commit"]["message"]})
+        refined_repo.update({'last_commit_sha': content['sha'], 'last_commit_message': content["commit"]["message"],
+                             'last_commit_author_name': content["commit"]["author"]["name"]})
 
 
 def refine_results(repo_details):
-    keys = ["name", "created_at", "avatar_url", "user_name", "commits_url"]
+    keys = ["name", "created_at", "avatar_url", "", "commits_url"]
     filtered_dict = {k:v for k, v in repo_details.items() if k in keys}
-    filtered_dict["owner"] = repo_details["owner"]["login"]
+    filtered_dict['created_at'] = strftime("%Y-%m-%d %H:%M:%S")
+    filtered_dict["login_url"] = repo_details["owner"]["html_url"]
+    filtered_dict["login_name"] = repo_details["owner"]["login"]
     filtered_dict["avatar_url"] = repo_details["owner"]["avatar_url"]
     return filtered_dict
