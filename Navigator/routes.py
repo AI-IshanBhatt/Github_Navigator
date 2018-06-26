98c4464e65bc0e75bf69f1f66dd51d75bbb82a13
Ishan Bhatt <39292911+AI-IshanBhatt@users.noreply.github.com>
2018-06-26 16:28:37 +0530

    Added error code handling for 500 and 404

--- Navigator/routes.py
+++ Navigator/routes.py
@@ -1,5 +1,5 @@
 from Navigator import app
-from flask import request, jsonify, render_template
+from flask import request, jsonify, render_template, make_response, abort
 import settings.development as settings
 from .utils import get_repositories
 
@@ -14,8 +14,24 @@ def get_repos(page_size=settings.PAGE_SIZE, order=settings.ORDER):
     app.logger.info("Searched for {}".format(search_term))
     # return jsonify(get_repositories(search_term, page_size, order))
     repositories = get_repositories(search_term, page_size, order)
-    return render_template("ReposList.html", repo_list=repositories, search_term=search_term)
+    if repositories:
+        return render_template("ReposList.html", repo_list=repositories, search_term=search_term)
+    else:
+        abort(500)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(500)
+def resource_unavailable(error):
+    return make_response("No Repository with given name", 500)
+
+
